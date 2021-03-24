5bbd6060025351bcc6a98343c29676f4166609e8
之一 <nicolanazwj@gmail.com>
2021-03-24 11:48:53 +0800

    update daily

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <!-- <column-list :list="list"></column-list> -->
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" calss="form-label">邮箱地址</label>
-        <validate-input :rules="emailRules" v-model="emailVal" placeholder="请输入邮箱地址" type="text"></validate-input>
-      </div>
-        <div class="mb-3">
-        <label for="exampleInputEmail1" calss="form-label">密码</label>
-        <validate-input :rules="passwordRules" v-model="passwordVal" placeholder="请输入密码" type="password"></validate-input>
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">保存</span>
-      </template>
-      <!-- <button type="submit" class="btn btn-primary">Submit</button> -->
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">2020 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
@@ -24,88 +21,24 @@ import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
 
 const currentUser:UserProps = {
-  isLogin: true,
+  isLogin: false,
   name: 'Nicolana',
   id: 1231
 }
-
-const emailReg = /^[0-9a-zA-Z_.-]+[@][0-9a-zA-Z_.-]+([.][a-zA-Z]+){1,2}$/
-
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 5,
-    title: 'test5的专栏',
-    description: '这是的test5专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
-
 export default defineComponent({
   name: 'App',
   components: {
-    // ColumnList,
-    GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GlobalHeader
   },
   setup () {
-    const emailVal = ref('')
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-
-    const passwordVal = ref('')
-    const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' }
-    ]
-
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
-
     const onFormSubmit = (result: boolean) => {
       console.log('1234', result)
     }
 
     return {
-      list: testData,
       currentUser: currentUser,
-      emailRef,
-      emailRules,
-      emailVal,
-      passwordVal,
-      passwordRules,
       onFormSubmit
     }
   }
